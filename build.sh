6dc242f448052a5d60e197c6b8595586e994661d
Luke Marsden <luke@clusterhq.com>
2015-03-01 14:08:46 +0000

    skip checking out linux into extant directory

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -xe
 cd /home/core
-git clone https://github.com/coreos/linux.git kernel-coreos-$(uname -r | sed 's/+$//')
-cd kernel-coreos-$(uname -r | sed 's/+$//')
-git checkout tags/v$(uname -r | sed 's/+$//')
-cd ..
+if [[ ! -d kernel-coreos-$(uname -r | sed 's/+$//') ]]; then
+    git clone https://github.com/coreos/linux.git kernel-coreos-$(uname -r | sed 's/+$//')
+    cd kernel-coreos-$(uname -r | sed 's/+$//')
+    git checkout tags/v$(uname -r | sed 's/+$//')
+    cd ..
+fi
 zcat /proc/config.gz > kernel-coreos-$(uname -r | sed 's/+$//')/.config
 sed -i -e '/CONFIG_SYSTEM_TRUSTED_KEYRING=y/d' kernel-coreos-$(uname -r | sed 's/+$//')/.config
 touch kernel-coreos-$(uname -r | sed 's/+$//')/.x509.list
