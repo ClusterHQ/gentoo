2ac60cb4220e76e290e0bac2b20287a6f87a0bad
Luke Marsden <luke@clusterhq.com>
2015-03-01 14:47:21 +0000

    allow building

--- build.sh
+++ build.sh
@@ -11,9 +11,14 @@ zcat /proc/config.gz > kernel-coreos-$(uname -r | sed 's/+$//')/.config
 sed -i -e '/CONFIG_SYSTEM_TRUSTED_KEYRING=y/d' kernel-coreos-$(uname -r | sed 's/+$//')/.config
 touch kernel-coreos-$(uname -r | sed 's/+$//')/.x509.list
 touch kernel-coreos-$(uname -r | sed 's/+$//')/bootengine.cpio
+# XXX possibly the following line is unnecessary because the tag already
+# includes Module.symvers
 #make -j8 -C $HOME/kernel-coreos-$(uname -r | sed 's/+$//') vmlinux modules
 
 # attempt to build latest git version of zfs and spl with "=[pkgname]-9999" syntax.
+echo "=sys-kernel/spl-9999" >> gentoo/etc/portage/package.unmask
+echo "=sys-fs/zfs-kmod-9999" >> gentoo/etc/portage/package.unmask
+
 env EXTRA_ECONF="--with-linux=$HOME/kernel-coreos-$(uname -r | sed 's/+$//')" KV_OUT_DIR=$HOME/kernel-coreos-$(uname -r | sed 's/+$//') emerge -j8 -1v =sys-kernel/spl-9999 =sys-fs/zfs-kmod-9999
 cp ~/kernel-coreos-$(uname -r | sed 's/+$//')/modules.{order,builtin} /home/core/gentoo/lib/modules/$(uname -r)/
 depmod -b ~/gentoo
