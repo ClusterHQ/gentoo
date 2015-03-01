cf064ea6bf1288f7c0dc7f101a5907920c2c62bb
Luke Marsden <luke@clusterhq.com>
2015-03-01 14:07:02 +0000

    try -j8

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ zcat /proc/config.gz > kernel-coreos-$(uname -r | sed 's/+$//')/.config
 sed -i -e '/CONFIG_SYSTEM_TRUSTED_KEYRING=y/d' kernel-coreos-$(uname -r | sed 's/+$//')/.config
 touch kernel-coreos-$(uname -r | sed 's/+$//')/.x509.list
 touch kernel-coreos-$(uname -r | sed 's/+$//')/bootengine.cpio
-make -C $HOME/kernel-coreos-$(uname -r | sed 's/+$//') vmlinux modules
-env EXTRA_ECONF="--with-linux=$HOME/kernel-coreos-$(uname -r | sed 's/+$//')" KV_OUT_DIR=$HOME/kernel-coreos-$(uname -r | sed 's/+$//') emerge -1v sys-kernel/spl sys-fs/zfs-kmod
+make -j8 -C $HOME/kernel-coreos-$(uname -r | sed 's/+$//') vmlinux modules
+env EXTRA_ECONF="--with-linux=$HOME/kernel-coreos-$(uname -r | sed 's/+$//')" KV_OUT_DIR=$HOME/kernel-coreos-$(uname -r | sed 's/+$//') emerge -j8 -1v sys-kernel/spl sys-fs/zfs-kmod
 cp ~/kernel-coreos-$(uname -r | sed 's/+$//')/modules.{order,builtin} /home/core/gentoo/lib/modules/$(uname -r)/
 depmod -b ~/gentoo
