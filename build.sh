1aa363c06c9e157da29208470feae7035554d388
Luke Marsden <luke@clusterhq.com>
2015-03-01 14:43:54 +0000

    attempt build from git

--- build.sh
+++ build.sh
@@ -6,11 +6,14 @@ if [[ ! -d kernel-coreos-$(uname -r | sed 's/+$//') ]]; then
     git checkout tags/v$(uname -r | sed 's/+$//')
     cd ..
 fi
+
 zcat /proc/config.gz > kernel-coreos-$(uname -r | sed 's/+$//')/.config
 sed -i -e '/CONFIG_SYSTEM_TRUSTED_KEYRING=y/d' kernel-coreos-$(uname -r | sed 's/+$//')/.config
 touch kernel-coreos-$(uname -r | sed 's/+$//')/.x509.list
 touch kernel-coreos-$(uname -r | sed 's/+$//')/bootengine.cpio
-make -j8 -C $HOME/kernel-coreos-$(uname -r | sed 's/+$//') vmlinux modules
-env EXTRA_ECONF="--with-linux=$HOME/kernel-coreos-$(uname -r | sed 's/+$//')" KV_OUT_DIR=$HOME/kernel-coreos-$(uname -r | sed 's/+$//') emerge -j8 -1v sys-kernel/spl sys-fs/zfs-kmod
+#make -j8 -C $HOME/kernel-coreos-$(uname -r | sed 's/+$//') vmlinux modules
+
+# attempt to build latest git version of zfs and spl with "=[pkgname]-9999" syntax.
+env EXTRA_ECONF="--with-linux=$HOME/kernel-coreos-$(uname -r | sed 's/+$//')" KV_OUT_DIR=$HOME/kernel-coreos-$(uname -r | sed 's/+$//') emerge -j8 -1v =sys-kernel/spl-9999 =sys-fs/zfs-kmod-9999
 cp ~/kernel-coreos-$(uname -r | sed 's/+$//')/modules.{order,builtin} /home/core/gentoo/lib/modules/$(uname -r)/
 depmod -b ~/gentoo
