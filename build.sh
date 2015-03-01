ea3ef04faa2088d198f05bf527911a520a9c6bc2
CoreOS Admin <core@ip-172-31-14-1.eu-central-1.compute.internal>
2015-03-01 14:01:53 +0000

    fix typo

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -xe
 cd /home/core
-git checkout https://github.com/coreos/linux.git kernel-coreos-$(uname -r | sed 's/+$//')
+git clone https://github.com/coreos/linux.git kernel-coreos-$(uname -r | sed 's/+$//')
 cd kernel-coreos-$(uname -r | sed 's/+$//')
 git checkout tags/v$(uname -r | sed 's/+$//')
 cd ..
