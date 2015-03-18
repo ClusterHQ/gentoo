667834209cd50a92aa5cd8340cc33adf29c98c67
Luke Marsden <luke@clusterhq.com>
2015-03-18 12:51:22 +0000

    build script that just fetches kernel sources from kernel.org... because coreos git is unreliable

--- build.sh
+++ build.sh
@@ -3,18 +3,18 @@
 EPREFIX=/home/core/gentoo
 
 cd /home/core
-if [[ ! -d kernel-coreos-$(uname -r | sed 's/+$//') ]]; then
-    git clone https://github.com/coreos/linux.git kernel-coreos-$(uname -r | sed 's/+$//')
-    cd kernel-coreos-$(uname -r | sed 's/+$//')
-    git checkout tags/v$(uname -r | sed 's/+$//')
+if [[ ! -d linux-$(uname -r | sed 's/+$//') ]]; then
+    wget https://www.kernel.org/pub/linux/kernel/v3.x/linux-$(uname -r | sed 's/+$//').tar.xz
+    tar xf linux-$(uname -r | sed 's/+$//').tar.xz
+    cd linux-$(uname -r | sed 's/+$//')
     cd ..
 fi
 
-zcat /proc/config.gz > kernel-coreos-$(uname -r | sed 's/+$//')/.config
-sed -i -e '/CONFIG_SYSTEM_TRUSTED_KEYRING=y/d' kernel-coreos-$(uname -r | sed 's/+$//')/.config
-touch kernel-coreos-$(uname -r | sed 's/+$//')/.x509.list
-touch kernel-coreos-$(uname -r | sed 's/+$//')/bootengine.cpio
-#make -j8 -C $HOME/kernel-coreos-$(uname -r | sed 's/+$//') vmlinux modules
+zcat /proc/config.gz > linux-$(uname -r | sed 's/+$//')/.config
+sed -i -e '/CONFIG_SYSTEM_TRUSTED_KEYRING=y/d' linux-$(uname -r | sed 's/+$//')/.config
+touch linux-$(uname -r | sed 's/+$//')/.x509.list
+touch linux-$(uname -r | sed 's/+$//')/bootengine.cpio
+#make -j8 -C $HOME/linux-$(uname -r | sed 's/+$//') vmlinux modules
 
 if [ -f ${EPREFIX}/etc/portage/package.unmask ]
 then
@@ -45,6 +45,6 @@ then
     echo "sys-fs/zfs **" >> ${EPREFIX}/etc/portage/package.accept_keywords/core-zfs-9999
 fi
 
-env KERNEL_DIR=$HOME/kernel-coreos-$(uname -r | sed 's/+$//') EXTRA_ECONF="--disable-systemd --with-udevdir=$HOME/gentoo/lib/udev --with-dracutdir=$HOME/gentoo/usr/lib/dracut --with-linux=$HOME/kernel-coreos-$(uname -r | sed 's/+$//')" KV_OUT_DIR=$HOME/kernel-coreos-$(uname -r | sed 's/+$//') emerge -j8 -1v =sys-kernel/spl-9999 =sys-fs/zfs-kmod-9999 =sys-fs/zfs-9999
-cp ~/kernel-coreos-$(uname -r | sed 's/+$//')/modules.{order,builtin} ${EPREFIX}/lib/modules/$(uname -r)/
+env KERNEL_DIR=$HOME/linux-$(uname -r | sed 's/+$//') EXTRA_ECONF="--disable-systemd --with-udevdir=$HOME/gentoo/lib/udev --with-dracutdir=$HOME/gentoo/usr/lib/dracut --with-linux=$HOME/linux-$(uname -r | sed 's/+$//')" KV_OUT_DIR=$HOME/linux-$(uname -r | sed 's/+$//') emerge -j8 -1v =sys-kernel/spl-9999 =sys-fs/zfs-kmod-9999 =sys-fs/zfs-9999
+cp ~/linux-$(uname -r | sed 's/+$//')/modules.{order,builtin} ${EPREFIX}/lib/modules/$(uname -r)/
 depmod -b ~/gentoo
