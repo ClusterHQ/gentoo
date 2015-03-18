2af8057cd72a45795e346dead2bafc5632e0c19a
Luke Marsden <luke@clusterhq.com>
2015-03-18 13:22:45 +0000

    note for later - how to pin specific revision of spl/zfs to build

--- build.sh
+++ build.sh
@@ -47,6 +47,11 @@ then
     echo "sys-fs/zfs **" >> ${EPREFIX}/etc/portage/package.accept_keywords/core-zfs-9999
 fi
 
+# The following should allow us to pin a specific revision of ZFS.
+#export spl_LIVE_COMMIT=...
+#export zfs_kmod_LIVE_COMMIT=...
+#export zfs_LIVE_COMMIT=...
+
 env KERNEL_DIR=$HOME/linux-$(uname -r | sed 's/+$//') EXTRA_ECONF="--disable-systemd --with-udevdir=$HOME/gentoo/lib/udev --with-dracutdir=$HOME/gentoo/usr/lib/dracut --with-linux=$HOME/linux-$(uname -r | sed 's/+$//')" KV_OUT_DIR=$HOME/linux-$(uname -r | sed 's/+$//') emerge -j8 -1v =sys-kernel/spl-9999 =sys-fs/zfs-kmod-9999 =sys-fs/zfs-9999
 cp ~/linux-$(uname -r | sed 's/+$//')/modules.{order,builtin} ${EPREFIX}/lib/modules/$(uname -r)/
 depmod -b ~/gentoo
