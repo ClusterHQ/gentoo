3ade6c21af072fd05d7ed4841e33498d579c9c20
Luke Marsden <luke@clusterhq.com>
2015-03-18 13:14:42 +0000

    try just running the shell script

--- metabuild.sh
+++ metabuild.sh
@@ -3,5 +3,4 @@
 # this file should be distributed separately from the repo...
 
 git clone https://github.com/clusterhq/gentoo
-gentoo/startprefix
-gentoo/build.sh
+gentoo/startprefix gentoo/build.sh
