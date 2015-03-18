71d446d93c8ca94dfd17438d51651e181579c3f9
Luke Marsden <luke@clusterhq.com>
2015-03-18 13:02:07 +0000

    create requisite directories

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ zcat /proc/config.gz > linux-$(uname -r | sed 's/+$//')/.config
 sed -i -e '/CONFIG_SYSTEM_TRUSTED_KEYRING=y/d' linux-$(uname -r | sed 's/+$//')/.config
 touch linux-$(uname -r | sed 's/+$//')/.x509.list
 touch linux-$(uname -r | sed 's/+$//')/bootengine.cpio
-#make -j8 -C $HOME/linux-$(uname -r | sed 's/+$//') vmlinux modules
+make -j8 -C $HOME/linux-$(uname -r | sed 's/+$//') vmlinux modules
 
 if [ -f ${EPREFIX}/etc/portage/package.unmask ]
 then
@@ -33,6 +33,7 @@ fi
 # attempt to build latest git version of zfs and spl with "=[pkgname]-9999" syntax.
 if [ ! -f ${EPREFIX}/etc/portage/package.unmask/core-zfs-9999 ]
 then
+    mkdir -p ${EPREFIX}/etc/portage/package.unmask
     echo "=sys-kernel/spl-9999" >> ${EPREFIX}/etc/portage/package.unmask/core-zfs-9999
     echo "=sys-fs/zfs-kmod-9999" >> ${EPREFIX}/etc/portage/package.unmask/core-zfs-9999
     echo "=sys-fs/zfs-9999" >> ${EPREFIX}/etc/portage/package.unmask/core-zfs-9999
@@ -40,6 +41,7 @@ fi
 
 if [ ! -f ${EPREFIX}/etc/portage/package.accept_keywords/core-zfs-9999 ]
 then
+    mkdir -p ${EPREFIX}/etc/portage/package.accept_keywords
     echo "sys-kernel/spl **" >> ${EPREFIX}/etc/portage/package.accept_keywords/core-zfs-9999
     echo "sys-fs/zfs-kmod **" >> ${EPREFIX}/etc/portage/package.accept_keywords/core-zfs-9999
     echo "sys-fs/zfs **" >> ${EPREFIX}/etc/portage/package.accept_keywords/core-zfs-9999
