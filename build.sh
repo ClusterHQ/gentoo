75dcd3075cb0a9eadde5715ff6df8582e027272a
Luke Marsden <luke@clusterhq.com>
2015-04-07 18:25:41 +0100

    upgrade to build so that it also creates a build artifact

--- build.sh
+++ build.sh
@@ -58,4 +58,28 @@ ebuild ${EPREFIX}/usr/portage/sys-kernel/spl/spl-9999.ebuild digest
 
 env KERNEL_DIR=$HOME/linux-$(uname -r | sed 's/+$//') EXTRA_ECONF="--disable-systemd --with-udevdir=$HOME/gentoo/lib/udev --with-dracutdir=$HOME/gentoo/usr/lib/dracut --with-linux=$HOME/linux-$(uname -r | sed 's/+$//')" KV_OUT_DIR=$HOME/linux-$(uname -r | sed 's/+$//') emerge -j8 -1v =sys-kernel/spl-9999 =sys-fs/zfs-kmod-9999 =sys-fs/zfs-9999
 cp ~/linux-$(uname -r | sed 's/+$//')/modules.{order,builtin} ${EPREFIX}/lib/modules/$(uname -r)/
-depmod -b ~/gentoo
+sudo depmod -b ~/gentoo
+
+sudo rmmod zunicode zavl zcommon znvpair spl zfs || true
+# Check that the new modules are loadable...
+sudo modprobe -d ~/gentoo zfs
+
+tar zcfv zfs-$(uname -r).tar.gz \
+	gentoo/usr/sbin/{zpool,zfs} \
+	gentoo/usr/lib/modules/$(uname -r) \
+	gentoo/lib \
+	gentoo/sbin \
+	gentoo/usr/lib/*.so* \
+	gentoo/usr/lib/libnvpair* \
+	gentoo/usr/lib/libuutil* \
+	gentoo/usr/lib/libzpool* \
+	gentoo/usr/lib/libzfs* \
+	gentoo/usr/lib/libm* \
+	gentoo/usr/lib/libdl* \
+	gentoo/usr/lib/libblkid* \
+	gentoo/usr/lib/librt* \
+	gentoo/usr/lib/libuuid* \
+	gentoo/usr/lib/libz* \
+	gentoo/usr/lib/libpthread* \
+	gentoo/usr/lib/libc* \
+	gentoo/usr/lib/ld-linux-x86-64*
